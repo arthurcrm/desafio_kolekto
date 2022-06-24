95c9510969411d249792955a9c0218251abd451c
Arthur <arthur.carneirontk@gmail.com>
2022-06-23 23:49:13 -0300

    Finish: LWC

--- kolekto/force-app/main/default/lwc/leadDetailComponent/leadDetailComponent.js
+++ kolekto/force-app/main/default/lwc/leadDetailComponent/leadDetailComponent.js
@@ -1,3 +1,53 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track, api } from "lwc";
+import getSavedData from "@salesforce/apex/LeadController.getSavedData";
+import { ShowToastEvent } from "lightning/platformShowToastEvent";
 
-export default class LeadDetailComponent extends LightningElement {}
+export default class LeadDetailComponent extends LightningElement {
+  @api recordId;
+
+  @track errorMessage = "";
+  @track error = false;
+  @track contacts = [];
+  @track accountName = "";
+  @track accountId = "";
+
+  showToast(title, variant, message) {
+    const event = new ShowToastEvent({
+      title: title,
+      variant: variant,
+      message: message
+    });
+    this.dispatchEvent(event);
+  }
+
+  async load() {
+    getSavedData({
+      leadId: this.recordId
+    })
+      .then((data) => {
+        console.log("DATA :: ", data);
+        if (data) {
+          if (data.error) {
+            console.log("Error ", data.message);
+            console.log("Error ", data.errorStackTrace);
+            this.errorMessage = data.message;
+            this.error = data.error;
+          } else {
+            const { accObj } = data;
+            this.accountName = accObj.Name;
+            this.accountId = accObj.Id;
+
+            this.contacts = accObj.Contacts;
+          }
+        } else {
+          this.error = true;
+        }
+      })
+      .catch((error) => {
+        console.log("error ", error);
+        console.log("Error received: code", error);
+        this.errorMessage = error;
+        this.error = true;
+      });
+  }
+}
